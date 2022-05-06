139b588164d06f7dde9546139bc862ea6c9e6773
wmz1024 <i@w03.cc>
2022-05-06 17:18:58 +0800

    aaa

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,40 +1,15 @@
 const workboxVersion = '5.1.3';
-
 importScripts(`https://storage.googleapis.com/workbox-cdn/releases/${workboxVersion}/workbox-sw.js`);
-
 workbox.core.setCacheNameDetails({
     prefix: "wlog"
 });
-
 workbox.core.skipWaiting();
-
 workbox.core.clientsClaim();
-
 workbox.precaching.cleanupOutdatedCaches();
-// 以下资源需要再开，不然可能缓存过多
-
-// Images
 workbox.routing.registerRoute(
-    /\.(?:png|jpg|jpeg|gif|bmp|webp|svg|ico)$/,
+    /\.(?:js|css|json|html)$/,
     new workbox.strategies.CacheFirst({
         cacheName: "images",
-        plugins: [
-            new workbox.expiration.ExpirationPlugin({
-                maxEntries: 1000,
-                maxAgeSeconds: 60 * 60 * 24 * 30
-            }),
-            new workbox.cacheableResponse.CacheableResponsePlugin({
-                statuses: [0, 200]
-            })
-        ]
-    })
-);
-
-// Fonts
-workbox.routing.registerRoute(
-    /\.(?:eot|ttf|woff|woff2)$/,
-    new workbox.strategies.CacheFirst({
-        cacheName: "fonts",
         plugins: [
             new workbox.expiration.ExpirationPlugin({
                 maxEntries: 1000,
