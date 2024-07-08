a16b04dbdd50fabaa669cfff73f9335195c9500d
Wmz1024 <82153828+wmz1024@users.noreply.github.com>
2024-07-08 23:02:32 +0800

    u

--- assets/js/main.nm.js
+++ assets/js/main.nm.js
@@ -27,7 +27,7 @@ window.addEventListener("load", async () => {
       .then(data => {
         document.getElementById("main").innerHTML = data.body;
         document.querySelector("title").innerHTML = data.name;
-        eval(data.js || "console.log('No JS could run')")
+        new Function(data.js || "console.log('[AwA.gs] No JS could run')");
       })
       .catch(err => console.log("Request Failed", err));
   } else {
