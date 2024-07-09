e1786f1f9f5ccd78b8d461c87799041bfb0fe4cf
Wmz1024 <82153828+wmz1024@users.noreply.github.com>
2024-07-09 09:52:11 +0800

    Update main.nm.js

--- assets/js/main.nm.js
+++ assets/js/main.nm.js
@@ -1,6 +1,7 @@
 window.addEventListener("load",async()=>{navigator.serviceWorker.register(`/sw.js?time=${new Date().getTime()}`).then(async(reg)=>{if(window.localStorage.getItem("install")!="true"){window.localStorage.setItem("install","true");setTimeout(()=>{console.log("helloWorld")},1000)}}).catch((err)=>{})});
 function run(data){
   document.write("<head></head>")
+  document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=document,i=p,c=\"\".concat(\"https:\"===e.location.protocol?\"https://\":\"http://\",\"sdk.51.la/js-sdk-pro.min.js\"),n=e.createElement(\"script\"),r=e.getElementsByTagName(\"script\")[0];n.type=\"text/javascript\",n.setAttribute(\"charset\",\"UTF-8\"),n.async=!0,n.src=c,n.id=\"LA_COLLECT\",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:\"KT7hdioJFlcNrs8g\",ck:\"KT7hdioJFlcNrs8g\",autoTrack:true});</script>")
     dbody=data.body;
     dname=data.name;
     drunjs=data.js || "console.log('[AwA.gs] No JS could run')";
